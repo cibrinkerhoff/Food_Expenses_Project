1f3adef457fc4efb0e1cf99bd9010801a615cbe9
Corbin Brinkerhoff <cib25@byu.edu>
2026-02-13 09:41:55 -0700

    LINE assumptions

---
+++ LINE_ASSUMPS.R
@@ -0,0 +1,20 @@
+food <- vroom :: vroom('FoodExpenses.txt')
+
+head(food)
+
+
+plot(food$Income, food$EatingOut, xlab = 'Income', ylab = 'Food Expenses', main = 'Food Expenses vs Income')
+
+#create resids
+model <- lm(EatingOut ~ Income, data = food)
+resids <- resid(model)
+
+#plot resids
+plot(food$Income, resids, xlab = 'Income', ylab = 'Residuals', main = 'Residuals vs Income')
+
+#check normality of resids
+qqnorm(resids)
+qqline(resids)
+
+hist(resids, xlab = 'Residuals', main = 'Histogram of Residuals')
+
