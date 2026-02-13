d42ed1423b5854b8d9ecd6a717da996409606fd2
Corbin Brinkerhoff <cib25@byu.edu>
2026-02-13 09:44:26 -0700

    add comments to assumptions

--- LINE_ASSUMPS.R
+++ LINE_ASSUMPS.R
@@ -4,6 +4,7 @@ head(food)
 
 
 plot(food$Income, food$EatingOut, xlab = 'Income', ylab = 'Food Expenses', main = 'Food Expenses vs Income')
+#line assumptions met
 
 #create resids
 model <- lm(EatingOut ~ Income, data = food)
@@ -11,10 +12,11 @@ resids <- resid(model)
 
 #plot resids
 plot(food$Income, resids, xlab = 'Income', ylab = 'Residuals', main = 'Residuals vs Income')
+#problems with heteroskedasticity
 
 #check normality of resids
 qqnorm(resids)
 qqline(resids)
-
 hist(resids, xlab = 'Residuals', main = 'Histogram of Residuals')
+#normality is met
 
